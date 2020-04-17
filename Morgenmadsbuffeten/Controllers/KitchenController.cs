c8a99370c41d66a5040f79e9dfc3b081ac6d8079
Mikkel-KR <Mikkel.kora@gmail.com>
2020-04-17 16:35:23 +0200

    Made controllers

---
+++ Morgenmadsbuffeten/Controllers/KitchenController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Morgenmadsbuffeten.Controllers
+{
+    /* Employee - Chef : Uses iPad */
+    //[Authorize("IsChef")]
+    public class KitchenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /* Given date, show guest-information (expected amount, checked-in amount, etc.) */
+        public IActionResult ShowOverview()
+        {
+            return View();
+        }
+    }
+}
